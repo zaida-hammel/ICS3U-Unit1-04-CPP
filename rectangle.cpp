9eec7907ec162ac6527fd773ef58ad18419efa32
Zaida Hammel <zzaida.hammel@mths.ca>
2022-09-19 21:43:30 +0000

    added rectangle.cpp

---
+++ rectangle.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Zaida Hammel All rights reserved
+//
+// Created by: Zaida Hammel
+// Created on: Sep 2022
+// This program calculates the area and perimeter of a rectangle
+//    with dimensions 4cm x 3cm
+
+#include <iostream>
+
+int main() {
+    std::cout << "A rectangle has the dimensions: " << std::endl;
+    std::cout << "4cm x 3cm" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Area is " << (4 * 3) << "cm²." << std::endl;
+    std::cout << "Perimeter is " << (2 * (4 + 3)) << "cm." << std::endl;
+    std::cout << std::endl;
+    std::cout << "Done." << std::endl;
+}
